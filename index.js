5fab628790749c6cc7186d8013e9a55f71d96d34
Lev <levdanneman@gmail.com>
2021-03-04 16:58:10 +0200

    heroku

--- index.js
+++ index.js
@@ -2,8 +2,6 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 
-// var S3 = require("aws-sdk").S3,
-//   S3S = require("s3-streams");
 const videosRoutes = require("./Routes/videos");
 
 app.use(express.json());
@@ -12,5 +10,5 @@ app.use(cors());
 app.use("/api/videos", videosRoutes);
 
 
-const port = 5000;
-app.listen(port, () => console.log(`Server is running on port ${port}`));
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
