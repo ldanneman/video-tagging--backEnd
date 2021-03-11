f36b925952ac5ee68e1951cadfa3391eec657c26
Lev <levdanneman@gmail.com>
2021-03-11 21:29:00 +0200

    vidoe download

--- Routes/videos.js
+++ Routes/videos.js
@@ -1,5 +1,7 @@
 const router = require("express").Router();
 const videoList = require("../aws");
+const axios = require("axios");
+const fs = require("fs");
 
 router.get("/", async (req, res, next) => {
   let videoURLs = videoList.list((videoURLs) => {
@@ -12,6 +14,18 @@ router.get("/", async (req, res, next) => {
       res.status(403).send(err, "there was an err");
     }
   });
+});
+
+router.post("/download", (req, res, next) => {
+  axios({
+    method: "get",
+    url: req.body.url,
+    responseType: "stream",
+  }).then(function (response) {
+    response.data.pipe(fs.createWriteStream("./Assets/test46.mp4"));
+  });
+  console.log("zzz", req.body.url);
+  // res.status(200).send(req.body);
 });
 
 module.exports = router;
--- index.js
+++ index.js
@@ -1,14 +1,38 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
 
+//Import Routes
 const videosRoutes = require("./Routes/videos");
 
+//Connection to Database
+// dotenv.config();
+// const mongoConnection = process.env.MONGODB_URI || process.env.DB_CONNECT;
+// const options = {
+//   useUnifiedTopology: true,
+//   useNewUrlParser: true,
+//   useFindAndModify: false,
+// };
+// mongoose.set("useFindAndModify", false);
+// mongoose.connect(
+//   process.env.MONGODB_URI || process.env.DB_CONNECT,
+//   { useUnifiedTopology: true, useNewUrlParser: true },
+//   () => console.log("connected to DB")
+// );
+
+// mongoose.connect(mongoConnection, options, () =>
+//   console.log("connected to DB")
+// );
+
+//Global Middlewares
 app.use(express.json());
 app.use(cors());
 
+//Routes Middlewares
 app.use("/api/videos", videosRoutes);
 
-
+//Server Initiation
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
