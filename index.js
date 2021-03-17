0de1dfccc10ec35fa6cb02ebae1ce1516ad2aa4d
Lev <levdanneman@gmail.com>
2021-03-18 00:34:48 +0200

    cors

--- index.js
+++ index.js
@@ -3,7 +3,14 @@ const app = express();
 const cors = require("cors");
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
-app.use(cors());
+// app.use(cors());
+app.use(function (req, res, next) {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+  res.setHeader("Access-Control-Allow-Credentials", true);
+  next();
+});
 //Import Routes
 const videosRoutes = require("./Routes/videos");
 
