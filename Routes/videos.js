33a5d7b87dc0ae971e848966d6ccde0625a3fb4c
Lev <levdanneman@gmail.com>
2021-03-31 00:40:53 +0300

    aws

--- Routes/videos.js
+++ Routes/videos.js
@@ -1,5 +1,5 @@
 const router = require("express").Router();
-const videoList = require("../aws");
+const videoList = require("../lib/aws");
 const axios = require("axios");
 const fs = require("fs");
 const path = require("path");
