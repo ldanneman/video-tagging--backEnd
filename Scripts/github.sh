7f1459b8969fc8ab073b774dadbba6ae906f0da0
Lev <levdanneman@gmail.com>
2021-03-31 17:50:00 +0300

    0.0.1

--- Scripts/github.sh
+++ Scripts/github.sh
@@ -1,8 +1,5 @@
-# echo "What's your name?"
-# read name
-# echo "Hello there, $name!"
 git add -A
 echo "type git message"
 read message
 git commit -m $message
-git push origin master
+git push origin --all
